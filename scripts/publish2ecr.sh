db286d5e95029caad9b0dabc37577f0de409d3be
Ofer Ravid <oferr@inuitive-tech.com>
2022-10-26 15:46:07 +0300

    fix publish

--- scripts/publish2ecr.sh
+++ scripts/publish2ecr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 NEW_TAG=$1
 
-aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 449814578585.dkr.ecr.eu-north-1.amazonaws.com
+aws ecr get-login-password --region eu-north-1 | docker login --username AWS -p $(aws ecr get-login-password --region eu-north-1) 449814578585.dkr.ecr.eu-north-1.amazonaws.com
 docker tag linkspace 449814578585.dkr.ecr.eu-north-1.amazonaws.com/linkspace:$NEW_TAG
 docker push 449814578585.dkr.ecr.eu-north-1.amazonaws.com/linkspace:$NEW_TAG
